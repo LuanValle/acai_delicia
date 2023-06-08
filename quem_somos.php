5d1f2fb5d9779272423b82ebea503a73c3d6163b
IsmaelFern <130618399+IsmaelFern@users.noreply.github.com>
2023-06-07 20:44:14 -0400

    Update quem_somos.php
    Feito por Ismael Fernandes

--- quem_somos.php
+++ quem_somos.php
@@ -4,7 +4,7 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" type="text/css" href="index2.css">
-		<title>Imperial rent a Car</title>
+		<title>Quem Somos_Açaí da Gleyce</title>
 	</head>
 <body>
 	<header>
@@ -39,28 +39,24 @@
 	<main class="conteudo">
 		<div>
 			<center>
-				<p> Imperio Rent a Car</p>
+				<p> Açaí da Gleyce</p>
 			</center>
 			
-			<img src="img/imagem1.png" class="img_frota">
+			<img src="img/marca.png" class="img_frota">
 
 		</div>
 		<div class="quem_somos_div">
 			<h2 class="quem_somos">
-				Olá, nós somos o Açaí da Gleyce!<br>
-				Um empreendimento completa de, <br>
-				que reúne diversas soluções inovadoras e eficientes.
+			Olá, nós somos o Açaí da Gleyce!<br>
+				Somos apaixonados pela arte de transformar o açaí em um verdadeiro deleite para nossos clientes. Com nosso açaí frozen, oferecemos uma experiência refrescante e nutritiva, que agrada a todos os gostos. <br>
+				Nossa equipe é composta por profissionais altamente qualificados e dedicados, que garantem um atendimento excepcional.<br>
 			</h2>
-			<center>
-			<h3 classe="quem_somos_h3">
-				Somos uma das maiores e mais completas plataformas de mobilidade do mundo.<br> 
-				Trazemos na nossa história resultados extraordinários, fruto da dedicação de mais<br> 
-				de 17 mil colaboradores empenhados em encantar pessoas e escrever com elas histórias incríveis. <br>
-				Com muita ousadia, protagonismo e inovação, oferecemos as melhores soluções de mobilidade, <br>
-				gerando valor para nossos colaboradores, investidores, clientes, parceiros e toda sociedade.
-			</h3>
-			</center>
 
+			<center>
+			<h1 classe="quem_somos_h1">
+				Aqui, cada cliente é tratado com carinho e atenção, porque para nós, a sua satisfação é o que realmente importa. Venha experimentar o melhor açaí de Manaus! <br>
+			</h1>
+			</center>
 		</div>
 
 		
